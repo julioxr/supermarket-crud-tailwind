91773731c6cac3d6e9dadadda6bbb74df512e22a
julio <julioroja987@gmail.com>
2022-11-03 01:14:21 -0300

    Se modificaron los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,42 @@
 import { useState } from "react";
 import Task from "./components/Task";
-import { BiTask } from "react-icons/bi";
+import { BsCart4 } from "react-icons/bs";
+import { TiPlus } from "react-icons/ti";
+import { RiArrowDownSLine } from "react-icons/ri";
 
 function App() {
     return (
         <>
-            {/* <div className="bg-primary-600 w-full text-center p-0 font-thin text-sm text-primary-50 py-2">
-                Hecho por @Julio
-            </div> */}
-            <header className="flex flex-col justify-center items-center mx-auto pt-6 h-[220px] bg-primary-300">
-                <div className="text-center">
-                    {/* <BiTask className="text-6xl w-full text-primary-800" /> */}
-                    <h1 className="text-5xl tracking-tight font-bold bg-clip-text text-transparent bg-gradient-to-br from-primary-700 to-primary-600 max-w-md mb-4">
-                        Supermarket checklist
-                    </h1>
-                    <span className="text-xl text-primary-50 font-semibold">
-                        Happy shopping!
-                    </span>
-
-                    <form action="#">
-                        <input
-                            type="text"
-                            placeholder="Agregue una tarea"
-                            className="rounded-sm font-thin placeholder:italic placeholder:text-slate-400 p-2 m-4 w-80 shadow-md shadow-primary-400"
-                        />
-                    </form>
-                </div>
+            <header className="flex flex-col items-center mx-auto h-[270px] bg-primary-300 text-center">
+                <section className="bg-primary-600 w-full h-2 mb-4 text-primary-50 py-2 text-2xl flex justify-center"></section>
+                <h1 className="text-5xl tracking-tight font-black pt-2 text-primary-50 max-w-md">
+                    Supermarket checklist
+                </h1>
+                <span className="text-xl text-primary-50 font-semibold mt-2 mb-1 ">
+                    Happy shopping!
+                </span>
+                <form action="#">
+                    <input
+                        type="text"
+                        placeholder="Agregue una tarea"
+                        className="rounded-lg font-thin placeholder:italic placeholder:text-slate-400 p-2.5 m-4 w-80 drop-shadow shadow-slate-500 text-base"
+                    />
+                </form>
             </header>
-            <main className="bg-gradient-to-b from-primary-300 to-primary-200 h-full overflow-y-hidden pt-2">
-                <section className="container flex flex-col items-center mx-auto overflow-y-hidden ">
-                    <Task icon="🍔" task="Comprar hamburguesas" />
-                    <Task icon="🍕" task="Comprar pizza" />
-                    <Task icon="🌭" task="Comprar pacho" />
-                    <Task icon="🍗" task="Comprar pollo" />
-                    <Task icon="🍿" task="Comprar papas fritas" />
-                    <Task icon="🥓" task="Comprar panceta" />
-                    <Task icon="🥟" task="Comprar empandas" />
-                </section>
-                <section></section>
+            <main className="bg-gradient-to-b from-primary-300 to-primary-200 h-full">
+                <div className="relative bg-primary-50/20 pt-[20px] rounded-2xl overflow-y-hidden w-[360px] h-[500px] mx-auto shadow-lg shadow-primary-400">
+                    <section className=" flex flex-col items-center gap-1.5 mx-auto">
+                        <Task icon="🍔" task="Comprar hamburguesas" />
+                        <Task icon="🍕" task="Comprar pizza" />
+                        <Task icon="🌭" task="Comprar pancho" />
+                        <Task icon="🍗" task="Comprar pollo" />
+                        <Task icon="🍿" task="Comprar papas fritas" />
+                        <Task icon="🥓" task="Comprar panceta" />
+                    </section>
+                    <div className="absolute bottom-4 left-1/2 -translate-x-1/2 ">
+                        <RiArrowDownSLine className="text-3xl text-primary-600 bottom-0" />
+                    </div>
+                </div>
             </main>
         </>
     );
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,13 +3,12 @@ import { TiDelete } from "react-icons/ti";
 
 const Task = ({ icon, task }) => {
     return (
-        <div className="flex justify-start rounded-sm bg-gradient-to-br from-primary-500 to-primary-600 text-primary-50 py-2 p-2 m-1 w-80 space-x-1 shadow-md shadow-primary-400 transition-all hover:my-3 hover:text-white hover:scale-105">
-            <span className="w-6 py-1">{icon}</span>
+        <div className="flex rounded-lg bg-gradient-to-l from-primary-400 to-primary-500 text-primary-50  p-2 m-1 w-80 space-x-1 drop-shadow-md shadow-primary-400 transition-all hover:my-3 hover:text-white hover:scale-105 cursor-pointer">
+            <span className="w-6 py-2">{icon}</span>
+
             <div className="flex w-full justify-between">
-                <p className=" text-sm cursor-pointer text-primary-100 py-2">
-                    {task}
-                </p>
-                <button>
+                <p className=" text-base text-primary-100 py-2">{task}</p>
+                <button className="text-2xl">
                     <TiDelete />
                 </button>
             </div>
