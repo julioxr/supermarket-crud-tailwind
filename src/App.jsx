65777cbc644e7f5da6d104253b1984f8274c7c2e
julio <julioroja987@gmail.com>
2022-11-03 22:39:09 -0300

    Se generan los items desde el input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from "react";
-import Task from "./components/Task";
-import { BsCart4 } from "react-icons/bs";
-import { TiPlus } from "react-icons/ti";
+import { useState, useEffect } from "react";
+import InputChecklist from "./components/InputChecklist";
 import { RiArrowDownSLine } from "react-icons/ri";
 
 function App() {
+    const initialState = {
+        id: 1,
+        icon: "🍕",
+        list: "Comprar pizzas",
+    };
+
+    const [checklist, setChecklist] = useState("");
+    const [checklists, setChecklists] = useState([initialState]);
+
+    const createChecklist = () => {
+        setChecklists([
+            ...checklists,
+            {
+                id: 2,
+                icon: "🍕",
+                list: checklist,
+            },
+        ]);
+    };
+
     return (
         <>
             <header className="flex flex-col items-center mx-auto h-[270px] bg-primary-300 text-center">
@@ -18,21 +36,39 @@ function App() {
                 <form action="#">
                     <input
                         type="text"
-                        placeholder="Agregue una tarea"
+                        placeholder="¿Qué necesitas comprar hoy?"
+                        value={checklist}
                         className="rounded-lg font-thin placeholder:italic placeholder:text-slate-400 p-2.5 m-4 w-80 drop-shadow shadow-slate-500 text-base"
+                        onChange={(e) => {
+                            setChecklist(e.target.value);
+                        }}
                     />
+                    <button
+                        type="button"
+                        onClick={() => createChecklist()}
+                        className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                    >
+                        Guardar
+                    </button>
                 </form>
             </header>
             <main className="bg-gradient-to-b from-primary-300 to-primary-200 h-full">
-                <div className="relative bg-primary-50/20 pt-[20px] rounded-2xl overflow-y-hidden w-[360px] h-[500px] mx-auto shadow-lg shadow-primary-400">
-                    <section className=" flex flex-col items-center gap-1.5 mx-auto">
-                        <Task icon="🍔" task="Comprar hamburguesas" />
-                        <Task icon="🍕" task="Comprar pizza" />
-                        <Task icon="🌭" task="Comprar pancho" />
-                        <Task icon="🍗" task="Comprar pollo" />
-                        <Task icon="🍿" task="Comprar papas fritas" />
-                        <Task icon="🥓" task="Comprar panceta" />
-                    </section>
+                <div className="relative bg-primary-200/20 pt-[20px] rounded-2xl w-[360px] h-[450px] mx-auto shadow-lg shadow-primary-400 ">
+                    <div className="w-[360px] h-[365px] overflow-y-scroll scrollbar-hide">
+                        <section
+                            id="checklist"
+                            className=" flex flex-col items-center gap-1.5 mx-auto"
+                        >
+                            {checklists &&
+                                checklists.map(({ id, icon, list }) => (
+                                    <InputChecklist
+                                        key={id}
+                                        icon={icon}
+                                        list={list}
+                                    />
+                                ))}
+                        </section>
+                    </div>
                     <div className="absolute bottom-4 left-1/2 -translate-x-1/2 ">
                         <RiArrowDownSLine className="text-3xl text-primary-600 bottom-0" />
                     </div>
--- src/components/InputChecklist.jsx
+++ src/components/InputChecklist.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { TiDelete } from "react-icons/ti";
 
-const Task = ({ icon, task }) => {
+const asd = ({ icon, list }) => {
     return (
         <div className="flex rounded-lg bg-gradient-to-l from-primary-400 to-primary-500 text-primary-50  p-2 m-1 w-80 space-x-1 drop-shadow-md shadow-primary-400 transition-all hover:my-3 hover:text-white hover:scale-105 cursor-pointer">
             <span className="w-6 py-2">{icon}</span>
 
             <div className="flex w-full justify-between">
-                <p className=" text-base text-primary-100 py-2">{task}</p>
+                <p className=" text-base text-primary-100 py-2">{list}</p>
                 <button className="text-2xl">
                     <TiDelete />
                 </button>
@@ -16,4 +16,4 @@ const Task = ({ icon, task }) => {
     );
 };
 
-export default Task;
+export default asd;
